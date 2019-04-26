fd6674c6b5d5e06022a2088795a3558fdbe7996f
Kuborros <kuborros@users.noreply.github.com>
2019-04-26 22:44:35 +0200

    I N D E N T A T I O N

--- src/main/java/com/kuborros/FurBotNeo/utils/audio/AudioPlayerSendHandler.java
+++ src/main/java/com/kuborros/FurBotNeo/utils/audio/AudioPlayerSendHandler.java
@@ -28,21 +28,21 @@ import com.sedmelluq.discord.lavaplayer.track.playback.AudioFrame;
 import net.dv8tion.jda.core.audio.AudioSendHandler;
 
 public class AudioPlayerSendHandler implements AudioSendHandler {
-  private final AudioPlayer audioPlayer;
-  private AudioFrame lastFrame;
+    private final AudioPlayer audioPlayer;
+    private AudioFrame lastFrame;
 
-  public AudioPlayerSendHandler(AudioPlayer audioPlayer) {
-    this.audioPlayer = audioPlayer;
-  }
-
-  @Override
-  public boolean canProvide() {
-    if (lastFrame == null) {
-      lastFrame = audioPlayer.provide();
+    public AudioPlayerSendHandler(AudioPlayer audioPlayer) {
+        this.audioPlayer = audioPlayer;
     }
 
-    return lastFrame != null;
-  }
+    @Override
+    public boolean canProvide() {
+        if (lastFrame == null) {
+            lastFrame = audioPlayer.provide();
+        }
+
+        return lastFrame != null;
+    }
 
     @Override
     public byte[] provide20MsAudio() {
