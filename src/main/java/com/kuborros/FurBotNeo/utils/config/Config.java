fec5d9b1be1cac8b9008fdc4026372805c4e7341
Kuborros <kuborros@users.noreply.github.com>
2019-11-26 21:31:18 +0100

    Confile class has been merged with Config class. Confile was a remnant of old codebase and currently only introduced needless confusion.

--- src/main/java/com/kuborros/FurBotNeo/utils/config/Config.java
+++ src/main/java/com/kuborros/FurBotNeo/utils/config/Config.java
@@ -1,5 +1,9 @@
 package com.kuborros.FurBotNeo.utils.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
 import java.util.Properties;
 
 
@@ -8,6 +12,8 @@ public class Config {
     private final String BOT_TOKEN;
     private final String OWNER_ID;
 
+    private static final Logger LOG = LoggerFactory.getLogger(Config.class);
+    private static final File CONFIG = new File("config.cfg");
 
     public String getBOT_TOKEN() {
         return BOT_TOKEN;
@@ -18,11 +24,76 @@ public class Config {
     }
 
     public Config() {
-        ConFile.ConFileCheck();
-        Properties properties = ConFile.getProperties();
+        ConFileCheck();
+        Properties properties = getProperties();
 
         BOT_TOKEN = properties != null ? properties.getProperty("BotToken") : "0";
         OWNER_ID = properties != null ? properties.getProperty("OwnerId") : "0";
+    }
+
+
+    private void ConFileCheck() {
+
+        try {
+            if (CONFIG.createNewFile()) {
+                Properties prop = new Properties();
+                OutputStream output = null;
+                LOG.info("Generating configuration file...");
+                try {
+
+                    output = new FileOutputStream(CONFIG);
+
+                    prop.setProperty("BotToken", "");
+                    prop.setProperty("OwnerId", "0");
+
+                    prop.store(output, null);
+
+                    LOG.info("You need to populate this file with your bot token and you userID!");
+
+                } catch (IOException e) {
+                    LOG.error("... and failed! ", e);
+                } finally {
+                    if (output != null) {
+                        try {
+                            output.close();
+                        } catch (IOException e) {
+                            LOG.error("Failed to close file: ", e);
+                        }
+                    }
+
+                }
+            }
+        } catch (IOException e) {
+            LOG.error("Error while creating configuration file! ", e);
+        }
+    }
+
+    private Properties getProperties() {
+
+        Properties prop = new Properties();
+        InputStream input = null;
+
+        try {
+
+            input = new FileInputStream(CONFIG);
+
+            prop.load(input);
+
+            return prop;
+
+        } catch (IOException e) {
+            LOG.error("Failed to obtain properties: ", e);
+            return null;
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    LOG.error("Failed to close file: ", e);
+                }
+            }
+        }
+
     }
 
 }
