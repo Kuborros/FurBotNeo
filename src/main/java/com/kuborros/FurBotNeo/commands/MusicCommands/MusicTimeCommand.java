a6b278341d8a1d96a1babfa31ab8f53025fef92a
Kuborros <kuborros@users.noreply.github.com>
2020-09-20 17:39:03 +0200

    Updated playtime command to use longs all the way. Updated old database queries to use prepared statements over *string snakes*

--- src/main/java/com/kuborros/FurBotNeo/commands/MusicCommands/MusicTimeCommand.java
+++ src/main/java/com/kuborros/FurBotNeo/commands/MusicCommands/MusicTimeCommand.java
@@ -60,7 +60,7 @@ public class MusicTimeCommand extends MusicCommand {
             } else {
                 val = val.trim();
             }
-            int seconds;
+            long seconds;
             try {
                 seconds = (min ? 60 : 1) * Integer.parseInt(val);
                 long milis = (seconds * 1000);
